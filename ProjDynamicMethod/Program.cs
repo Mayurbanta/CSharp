f7d6e67f14aab4be89f66570111a53971f9d9aed
Mayur Banta <mayurbanta@gmail.com>
2018-10-01 12:35:29 +0530

    C# - Project for creating instance using reflection, dynamic method and direct reference.

--- ProjDynamicMethod/Program.cs
+++ ProjDynamicMethod/Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace ProjDynamicMethod
 {
+    using System.Diagnostics;
     class Program
     {
         static void Main(string[] args)
         {
+            bool res;
+            long ticks = 0;
+            Stopwatch stopwatch = new Stopwatch();
+
+            Worker worker = new Worker(10);
+
+            //By Reflection-------------------------------------*
+            stopwatch.Start();
+            res = worker.InstanceByReflection();
+            stopwatch.Stop();
+            ticks = 0;
+            ticks = stopwatch.ElapsedTicks;
+            System.Console.WriteLine($"By Reflection: {ticks.ToString()}");
+
+
+            //By Dynamic Method-------------------------------------
+            stopwatch.Restart();
+            res = worker.InstanceByReflection();
+            stopwatch.Stop();
+            ticks = 0;
+            ticks = stopwatch.ElapsedTicks;
+            System.Console.WriteLine($"By Dynamic Method: {ticks.ToString()}");
+
+
+            //By Direct Reference-------------------------------------
+            stopwatch.Restart();
+            res = worker.InstanceByDirectReference();
+            stopwatch.Stop();
+            ticks = 0;
+            ticks = stopwatch.ElapsedTicks;
+            System.Console.WriteLine($"By Direct Reference: {ticks.ToString()}");
+
+            System.Console.Read();
         }
     }
 }
---
+++ ProjDynamicMethod/Worker.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ProjDynamicMethod
+{
+    public class Worker
+    {
+        private readonly int ITERATIONS;
+
+        public Worker(int iterations)
+        {
+            ITERATIONS = iterations;
+        }
+
+        public delegate object ConstructorDelegate();
+
+        protected ConstructorDelegate GetConstructor(string TypeName)
+        {
+            //Get the defaul constructor of the type
+            Type t = Type.GetType(TypeName);
+            ConstructorInfo ctor = t.GetConstructor(new Type[0]);
+
+            // create a new dynamic method that constructs and returns the type
+            string methodName = t.Name + "Ctor";
+            DynamicMethod dm = new DynamicMethod(methodName, t, new Type[0], typeof(Activator));
+            ILGenerator lgen = dm.GetILGenerator();
+            lgen.Emit(OpCodes.Newobj, ctor);
+            lgen.Emit(OpCodes.Ret);
+
+            //add delegate to dictionary and return
+            ConstructorDelegate creator = (ConstructorDelegate)dm.CreateDelegate(typeof(ConstructorDelegate));
+
+            //return a delegate to the method
+            return creator;
+
+        }
+
+        public bool InstanceByReflection()
+        {
+            var type = Type.GetType("System.Text.StringBuilder");
+            for (int i = 0; i < ITERATIONS; i++)
+            {
+                var obj = Activator.CreateInstance(type);
+                if (obj.GetType() != typeof(System.Text.StringBuilder))
+                {
+                    throw new InvalidOperationException("Constructed Object is not a StringBuilder");
+                }
+            }
+
+            return true;
+        }
+
+        public bool InstanceByDynamicMEthod()
+        {
+
+            var constructor = GetConstructor("System.Text.StringBuilder");
+            for (int i = 0; i < ITERATIONS; i++)
+            {
+                var obj = constructor();
+                if (obj.GetType() != typeof(System.Text.StringBuilder))
+                {
+                    throw new InvalidOperationException("Constructed Object is not a StringBuilder");
+                }
+            }
+
+            return true;
+        }
+
+        public bool InstanceByDirectReference()
+        {
+            for (int i = 0; i < ITERATIONS; i++)
+            {
+                var obj = new System.Text.StringBuilder();
+                if (obj.GetType() != typeof(System.Text.StringBuilder))
+                {
+                    throw new InvalidOperationException("Constructed Object is not a StringBuilder");
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
